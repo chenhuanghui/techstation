56f84e41fbc2147cea4806ea476da6fc31d42e44
Huy Tran <huytran@enZos-MacBook-Pro.local>
2018-10-07 15:13:30 +0700

    add jquery

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import jQuery from 'jquery'
+window.jQuery = jQuery
+window.$ = jQuery
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
