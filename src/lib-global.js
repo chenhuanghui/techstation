d66c23a108e083614c9e01de0629d9e460776369
Huy Tran <huytran@enZos-MacBook-Pro.local>
2018-10-09 00:02:32 +0700

    add lib javascript

--- src/lib-global.js
+++ src/lib-global.js
@@ -10,6 +10,23 @@ import './assets/css/slick-style.css'
 import './assets/css/animate.min.css'
 import './assets/css/colors/blue.css'
 
-export default {
+// import Tether from 'tether'
+// import Bootstrap from './assets/js/bootstrap.min.js'
+import BootstrapVue from 'bootstrap-vue';
+import JqMigrate from './assets/js/jquery-migrate.min'
+import HideMaxListItem from './assets/js/hidemaxlistitem.min'
+import JqUI from './assets/js/jquery-ui.min'
+import JqEasing from './assets/js/jquery.easing.min'
+import ScroolUp from './assets/js/scrollup.min'
+import JqWaypoint from './assets/js/jquery.waypoints.min'
+import WaypointSticky from './assets/js/waypoints-sticky.min'
+import Pace from './assets/js/pace.min'
+import Slick from './assets/js/slick.min'
+import Scripts from './assets/js/scripts'
 
+export default {
+  install (Vue) {
+    // Vue.use(Tether)
+    Vue.use(BootstrapVue)
+  }
 }
