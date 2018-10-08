c03e079730814a4ca7f71d2b5924dcb2436feb2b
Huy Tran <huytran@enZos-MacBook-Pro.local>
2018-10-08 23:13:16 +0700

    add HomeSTATION.vue

---
+++ src/components/HomeStation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home-station">
+    {{msg}}
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HomeStation',
+  data () {
+    return {
+      msg: 'Welcome to HomeStation'
+    }
+  }
+}
+</script>
