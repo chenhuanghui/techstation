ce31be80f3aff4e115ecc58d4a777a9eb9d5f3ea
Huy Tran <huytran@enZos-MacBook-Pro.local>
2018-10-09 23:49:16 +0700

    remove others import component in HomeStation

--- src/components/HomeStation.vue
+++ src/components/HomeStation.vue
@@ -4,13 +4,7 @@
   </div>
 </template>
 
-
 <script>
-import TopBar from '@/components/child-components/TopBar'
-import StickyWrapperDesktop from '@/components/child-components/StickyWrapperDesktop'
-import NavigatorDesktop from '@/components/child-components/NavigatorDesktop'
-import NavigatorMobile from '@/components/child-components/NavigatorMobile'
-import StickWrapperMobile from '@/components/child-components/StickyWrapperMobile'
 
 import Header from '@/components/child-components/Header'
 
